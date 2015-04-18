b3d007c0b2274af1ff6c48d9d8924479c2995110
Hal Finkel <hfinkel@anl.gov>
2015-04-18 17:38:02 -0500

    Merging...

--- lib/CodeGen/CodeGenModule.cpp
+++ lib/CodeGen/CodeGenModule.cpp
@@ -1478,31 +1478,6 @@ void CodeGenModule::EmitGlobal(GlobalDecl GD) {
                               /*DontDefer=*/false);
       return;
     }
-
-    if (llvm::GlobalValue *GV = GetGlobalValue(getMangledName(GD)))
-      if (!GV->isDeclaration()) {
-        // This can happen for OpenMP simd functions; ignore it in that case...
-        bool IgnoreDup = false;
-        if (const CXXMethodDecl *MD = dyn_cast_or_null<CXXMethodDecl>(GD.getDecl())) {
-          const CXXRecordDecl *Parent = MD->getParent();
-          for (DeclContext::decl_iterator DI = Parent->decls_begin(),
-                                          DE = Parent->decls_end();
-                                          DI != DE; ++DI) {
-            if (dyn_cast_or_null<OMPDeclareSimdDecl>(*DI)) {
-              IgnoreDup = true;
-              break;
-            }
-          }
-        }
-
-        if (!IgnoreDup) {
-          getDiags().Report(FD->getLocation(), diag::err_duplicate_mangled_name);
-          GlobalDecl OldGD = Manglings.lookup(GV->getName());
-          if (auto *Prev = OldGD.getDecl())
-            getDiags().Report(Prev->getLocation(), diag::note_previous_definition);
-          return;
-        }
-      }
   } else {
     const auto *VD = cast<VarDecl>(Global);
     assert(VD->isFileVarDecl() && "Cannot emit local var decl as global.");
@@ -2611,6 +2586,14 @@ void CodeGenModule::EmitGlobalFunctionDefinition(GlobalDecl GD,
     } else {
       GV = cast<llvm::GlobalValue>(C);
     }
+  }
+
+  if (!GV->isDeclaration()) {
+    getDiags().Report(D->getLocation(), diag::err_duplicate_mangled_name);
+    GlobalDecl OldGD = Manglings.lookup(GV->getName());
+    if (auto *Prev = OldGD.getDecl())
+      getDiags().Report(Prev->getLocation(), diag::note_previous_definition);
+    return;
   }
 
   if (GV->getType()->getElementType() != Ty) {
