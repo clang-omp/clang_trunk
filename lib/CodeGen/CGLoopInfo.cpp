42127803b6bd345bda815b257c84028ef2e75c68
Hal Finkel <hfinkel@anl.gov>
2015-06-11 06:56:26 -0500

    Merging...

--- lib/CodeGen/CGLoopInfo.cpp
+++ lib/CodeGen/CGLoopInfo.cpp
@@ -13,8 +13,8 @@
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Metadata.h"
-using namespace clang;
-using namespace CodeGen;
+using namespace clang::CodeGen;
+using namespace llvm;
 
 static llvm::MDNode *CreateMetadata(llvm::LLVMContext &Ctx,
                                     const LoopAttributes &Attrs) {
